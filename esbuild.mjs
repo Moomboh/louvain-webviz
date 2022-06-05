6e2a698cb4167ebc954be300022045cb1aaff2ef
Elias Kahl <contact@elias.works>
2022-06-05 16:16:04 +0000

    chore: setup build and dev environment

---
+++ esbuild.mjs
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+/* eslint-env node */
+import esbuild from "esbuild";
+import { htmlPlugin } from "@craftamap/esbuild-plugin-html";
+import { readFileSync } from "fs";
+
+const isDev = process.env.NODE_ENV === "development";
+const isWatch = process.argv.includes("--watch");
+
+await esbuild.build({
+  entryPoints: ["src/index.ts"],
+  outdir: "dist",
+  sourcemap: isDev,
+  bundle: true,
+  metafile: true,
+  plugins: [
+    htmlPlugin({
+      files: [
+        {
+          entryPoints: ["src/index.ts"],
+          filename: "index.html",
+          htmlTemplate: readFileSync("src/index.html", "utf8"),
+        },
+      ],
+    }),
+  ],
+  watch: isWatch && {
+    onRebuild(error, result) {
+      if (error) console.error("watch build failed:", error);
+      else
+        console.log(
+          "watch build succeeded:\n",
+          Object.keys(result.metafile.outputs).join("\n")
+        );
+    },
+  },
+});
